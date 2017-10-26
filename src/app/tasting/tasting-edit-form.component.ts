dec91f6f04be884ab738e699b2f3bbe0b238d8b6
Jon Hobbs <jon.hobbs@minnykid.com>
2017-10-26 12:53:18 -0500

    Tweaked form labels
    made 'em bold!

--- src/app/tasting/tasting-edit-form.component.ts
+++ src/app/tasting/tasting-edit-form.component.ts
@@ -3,8 +3,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tasting-edit-form',
-  templateUrl: './tasting-edit-form.template.html',
-  styleUrls: ['./tasting-edit-form.component.css']
+  templateUrl: './tasting-edit-form.template.html'
 })
 
 export class TastingEditFormComponent {
